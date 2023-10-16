4189ec53c36536a597a28a1d37ecac15ab127ee2
MartinDPB <martindipardo@hotmail.com>
2023-10-16 18:00:05 -0300

    v 1.7

--- js/footer.js
+++ js/footer.js
@@ -1,77 +1,64 @@
 const d = document,
-$footer = d.querySelector("footer"),
-$txtPais = "Argentina.",
-$txtCopy = "©Di Pardo. Todos los derechos reservados.",
-$txtTerms = "Términos y Condiciones",
-$txtPriv = "Política de privacidad y cookies";
-import scroll from "./scrollBar.js";
-export default function footer(){
-    let $sectionRedes = d.createElement("section"),
-    $twitter = d.createElement("i"),
-    $twitterA = d.createElement("a"),
-    $facebook = d.createElement("i"),
-    $facebookA = d.createElement("a"),
-    $youtube = d.createElement("i"),
-    $youtubeA = d.createElement("a"),
-    $instagram = d.createElement("i"),
-    $instagramA = d.createElement("a"),
-    $scrollbar = d.createElement("section"),
-    $scroll = d.createElement("div"),
+  $footer = d.querySelector("footer"),
+  $footerText = [
+    "Argentina.",
+    "©Di Pardo. Todos los derechos reservados.",
+    "Términos y Condiciones",
+    "Política de privacidad y cookies",
+  ];
+
+export default function footer() {
+  let $footerRedes = [
+    {
+      icon: "fa-brands fa-facebook",
+      url: "https://facebook.com/Nike",
+      nombre: "facebook",
+    },
+    {
+      icon: "fa-brands fa-youtube",
+      url: "https://youtube.com/Nike",
+      nombre: "youtube",
+    },
+    {
+      icon: "fa-brands fa-instagram",
+      url: "https://instagram.com/Nike",
+      nombre: "instagram",
+    },
+    {
+      icon: "fa-brands fa-square-x-twitter",
+      url: "https://twitter.com/Nike",
+      nombre: "twitter",
+    },
+  ];
+
+  let $sectionRedes = d.createElement("section"),
     $informacion = d.createElement("section"),
-    $pPais = d.createElement("p"),
-    $pCopy = d.createElement("p"),
-    $pTerms = d.createElement("p"),
-    $pPriv = d.createElement("P");
-    $facebook.className = "fa-brands fa-facebook";
-    $youtube.className = "fa-brands fa-youtube";
-    $instagram.className = "fa-brands fa-instagram";
-    $twitter.className = "fa-brands fa-square-x-twitter";
-    scroll();
+    $fragment = d.createDocumentFragment();
 
+  $sectionRedes.classList.add("redes");
 
+  $footerRedes.forEach((el) => {
+    const $a = d.createElement("a");
+    const $i = d.createElement("i");
+    $a.setAttribute("href", el.url);
+    $a.setAttribute("target", "_blank");
+    $i.className = el.icon;
+    $i.setAttribute("alt", `${el.nombre} icon`);
+    $a.appendChild($i);
+    const $clone = $a.cloneNode($a, true);
+    $sectionRedes.appendChild($clone);
+  });
+  const $ul = d.createElement("ul");
 
-    $footer.appendChild($sectionRedes);
-    $footer.appendChild($scrollbar);
-    $footer.appendChild($informacion);
+  $footerText.forEach((el) => {
+    const $li = d.createElement("li");
+    $li.innerHTML = `<p>${el}</p>`;
+    const $clone = $li.cloneNode($li, true);
+    $ul.insertAdjacentElement("beforeend", $clone);
+  });
+  $informacion.appendChild($ul);
 
-    $sectionRedes.classList.add("redes");
-
-    $sectionRedes.appendChild($twitterA);
-    $twitterA.appendChild($twitter);
-    $twitterA.setAttribute("href","https://twitter.com/Nike");
-    $twitterA.setAttribute("target","_blank");
-    $twitter.setAttribute("alt","twitter icon");
-
-    $sectionRedes.appendChild($facebookA);
-    $facebookA.appendChild($facebook);
-    $facebookA.setAttribute("href","https://twitter.com/Nike");
-    $facebookA.setAttribute("target","_blank");
-    $facebook.setAttribute("alt","Facebook icon");
-
-    $sectionRedes.appendChild($youtubeA);
-    $youtubeA.appendChild($youtube);
-    $youtubeA.setAttribute("href","https://twitter.com/Nike");
-    $youtubeA.setAttribute("target","_blank");
-    $youtube.setAttribute("alt","YouTube icon");
-
-    $sectionRedes.appendChild($instagramA);
-    $instagramA.appendChild($instagram);
-    $instagramA.setAttribute("href","https://twitter.com/Nike");
-    $instagramA.setAttribute("target","_blank");
-    $instagram.setAttribute("alt","Instagram icon");
-    
-
-    $informacion.appendChild($pPais);
-    $pPais.innerText = $txtPais;
-
-    $informacion.appendChild($pCopy);
-    $pPais.innerText = $txtCopy;
-
-    $informacion.appendChild($pTerms);
-    $pPais.innerText = $txtTerms;
-
-    $informacion.appendChild($pPriv);
-    $pPais.innerText = $txtPriv;
-
-    
+  $fragment.appendChild($informacion);
+  $fragment.appendChild($sectionRedes);
+  $footer.appendChild($fragment);
 }
